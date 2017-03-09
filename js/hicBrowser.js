a3426b12d093b9cdce05e629114c4996aa0e27e3
Jim Robinson <jrobinso@broadinstitute.org>
2017-03-09 11:16:59 -0800

    Move hic.State to hic.Browser file

--- js/hicBrowser.js
+++ js/hicBrowser.js
@@ -393,6 +393,19 @@ var hic = (function (hic) {
             return results[1];
     }
 
+    hic.State = function(chr1, chr2, zoom, x, y, pixelSize) {
+        this.chr1 = chr1;
+        this.chr2 = chr2;
+        this.zoom = zoom;
+        this.x = x;
+        this.y = y;
+        this.pixelSize = pixelSize;
+    };
+
+    hic.State.prototype.toString = function () {
+        return "" + this.chr1 + "," + this.chr2 + "," + this.zoom + "," + this.x + "," + this.y + "," + this.pixelSize;
+    };
+
     return hic;
 
 })
