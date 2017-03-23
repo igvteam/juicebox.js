39fd5e1f195772de26b8ed162d5f70d831f14849
Jim Robinson <jrobinso@broadinstitute.org>
2017-03-22 18:54:24 -0700

    Add tooltip text to normalization options

--- js/normalizationWidget.js
+++ js/normalizationWidget.js
@@ -3,6 +3,19 @@
  */
 var hic = (function (hic) {
 
+    var labels = {
+        VC: "Coverage",
+        VC_SQRT: "Coverage (Sqrt)",
+        KR: "Balanced",
+        INTER_VC: "Interchromosomal Coverage",
+        INTER_VC_SQRT: "Interchromosomal Coverage (Sqrt)",
+        INTER_KR: "Interchromosomal Balanced",
+        GW_VC: "Genome-wide Coverage",
+        GW_VC_SQRT: "Genome-wide Coverage (Sqrt)",
+        GW_KR: "Genome-wide Balanced",
+
+    }
+
     hic.NormalizationWidget = function (browser) {
         var self = this,
             $label,
@@ -16,7 +29,7 @@ var hic = (function (hic) {
 
         this.$normalization_selector = $('<select name="select">');
         this.$normalization_selector.on('change', function (e) {
-            self.browser.setNormalization( $(this).val() );
+            self.browser.setNormalization($(this).val());
         });
 
         this.$normalization_selector.attr('name', 'dataset_selector');
@@ -42,7 +55,7 @@ var hic = (function (hic) {
 
     };
 
-    hic.NormalizationWidget.prototype.respondToDataLoadWithDataset = function(dataset) {
+    hic.NormalizationWidget.prototype.respondToDataLoadWithDataset = function (dataset) {
 
         var selected,
             normalizationTypes,
@@ -51,7 +64,10 @@ var hic = (function (hic) {
         selected = false;
         normalizationTypes = dataset.normalizationTypes;
         elements = _.map(normalizationTypes, function (normalization) {
-            return '<option' + ' value=' + normalization + (selected ? ' selected' : '') + '>' + normalization + '</option>';
+            var label = labels[normalization];
+            return '<option' +
+                (label === undefined ? '' : ' title = "' + label + '" ') +
+                ' value=' + normalization + (selected ? ' selected' : '') + '>' + normalization + '</option>';
         });
 
         this.$normalization_selector.empty();
