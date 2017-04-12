0a23738a3287f7ec2aff0405a90e5aa967441fb9
Douglass Turner <douglass.turner@gmail.com>
2017-04-12 17:16:06 -0400

    trackCore.js. Added knownFileTypes

--- js/igv/trackCore.js
+++ js/igv/trackCore.js
@@ -28,6 +28,8 @@
 
 var igv = (function (igv) {
 
+    var knownFileExtensions = new Set(["narrowpeak", "broadpeak", "peaks", "bedgraph", "wig", "gff3", "gff",
+        "gtf", "aneu", "fusionjuncspan", "refflat", "seg", "bed", "vcf", "bb", "bigbed", "bw", "bigwig", "bam", "tdf"]);
 
     igv.createTrackWithConfiguration = function(conf) {
 
