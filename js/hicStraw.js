6422e0d57ae5404c3d5d33a4810df9e976d0d15d
Jim Robinson <jrobinso@broadinstitute.org>
2017-08-17 20:53:00 -0700

    Disable unfinished "Straw" code

--- js/hicStraw.js
+++ js/hicStraw.js
@@ -21,89 +21,89 @@
  *
  */
 
-var hic = (function (hic) {
-
-    hic.Straw = function (urlOrFile) {
-
-        this.url = urlOrFile
-
-        this.reader = new hic.Reader({url: this.url, loadFragData: true});
-
-    }
-
-//straw <NONE/VC/VC_SQRT/KR> <hicFile> <chr1>[:x1:x2] <chr2>[:y1:y2] <BP/FRAG> <binsize>
-    ​
-    hic.Straw.getContactRecords = function (normalization, region1, region2, units, binsize) {
-
-        return new Promise(function (success, reject) {
-            var self = this,
-                chr1 = region1.chr2,
-                chr2 = region2.chr2,
-                bpx1 = region1.start,
-                bpx2 = region1.end,
-                bpy1 = region2.start,
-                bpy2 = region2.end;
-
-            getDataset.call(this).then(function (dataset) {
-
-                dataset.getMatrix(state.chr1, state.chr2)
-
-                    .then(function (matrix) {
-
-                        var zd = matrix.bpZoomData[state.zoom],
-                            blockBinCount = zd.blockBinCount,   // Dimension in bins of a block (width = height = blockBinCount)
-                            pixelSizeInt = Math.max(1, Math.floor(state.pixelSize)),
-                            col1 = bpx1 === undefined ? 0 : Math.floor(bpx1 / blockBinCount),
-                            col2 = bpx1 === undefined ? zd.blockColumnCount : Math.floor(bpx2 / blockBinCount),
-                            row1 = Math.floor(bpy1 / blockBinCount),
-                            row2 = Math.floor(bpy2 / blockBinCount),
-                            row, column, sameChr, blockNumber,
-                            promises = [];
-
-                        sameChr = chr1 === chr2;
-
-                        for (row = row1; row <= row2; row++) {
-                            for (column = col1; column <= col2; column++) {
-                                if (sameChr && row < column) {
-                                    blockNumber = column * zd.blockColumnCount + row;
-                                }
-                                else {
-                                    blockNumber = row * zd.blockColumnCount + column;
-                                }
-
-                                promises.push(self.dataset.getNormalizedBlock(zd, blockNumber, normalization))
-                            }
-                        }
-
-                        Promise.all(promises)
-                            .then(function (blocks) {
-
-                            })
-                            .catch(reject)
-
-                    })
-                    .catch(function (error) {
-                        self.stopSpinner();
-                        self.updating = false;
-                        console.error(error);
-                    })
-            })
-                .catch(reject);
-        });
-    }
-
-
-    function getDataset() {
-
-        if (this.dataset) {
-            return Promise.resolve(this.dataset);
-        }
-        else {
-            return this.reader.loadDataset();
-        }
-
-    }
-
-
-    return hic;
-})(hic || {});
+// var hic = (function (hic) {
+//
+//     hic.Straw = function (urlOrFile) {
+//
+//         this.url = urlOrFile;
+//
+//         this.reader = new hic.HicReader({url: this.url, loadFragData: true});
+//
+//     }
+//
+// //straw <NONE/VC/VC_SQRT/KR> <hicFile> <chr1>[:x1:x2] <chr2>[:y1:y2] <BP/FRAG> <binsize>
+//     ​
+//     hic.Straw.getContactRecords = function (normalization, region1, region2, units, binsize) {
+//
+//         return new Promise(function (success, reject) {
+//             var self = this,
+//                 chr1 = region1.chr2,
+//                 chr2 = region2.chr2,
+//                 bpx1 = region1.start,
+//                 bpx2 = region1.end,
+//                 bpy1 = region2.start,
+//                 bpy2 = region2.end;
+//
+//             getDataset.call(this).then(function (dataset) {
+//
+//                 dataset.getMatrix(state.chr1, state.chr2)
+//
+//                     .then(function (matrix) {
+//
+//                         var zd = matrix.bpZoomData[state.zoom],
+//                             blockBinCount = zd.blockBinCount,   // Dimension in bins of a block (width = height = blockBinCount)
+//                             pixelSizeInt = Math.max(1, Math.floor(state.pixelSize)),
+//                             col1 = bpx1 === undefined ? 0 : Math.floor(bpx1 / blockBinCount),
+//                             col2 = bpx1 === undefined ? zd.blockColumnCount : Math.floor(bpx2 / blockBinCount),
+//                             row1 = Math.floor(bpy1 / blockBinCount),
+//                             row2 = Math.floor(bpy2 / blockBinCount),
+//                             row, column, sameChr, blockNumber,
+//                             promises = [];
+//
+//                         sameChr = chr1 === chr2;
+//
+//                         for (row = row1; row <= row2; row++) {
+//                             for (column = col1; column <= col2; column++) {
+//                                 if (sameChr && row < column) {
+//                                     blockNumber = column * zd.blockColumnCount + row;
+//                                 }
+//                                 else {
+//                                     blockNumber = row * zd.blockColumnCount + column;
+//                                 }
+//
+//                                 promises.push(self.dataset.getNormalizedBlock(zd, blockNumber, normalization))
+//                             }
+//                         }
+//
+//                         Promise.all(promises)
+//                             .then(function (blocks) {
+//
+//                             })
+//                             .catch(reject)
+//
+//                     })
+//                     .catch(function (error) {
+//                         self.stopSpinner();
+//                         self.updating = false;
+//                         console.error(error);
+//                     })
+//             })
+//                 .catch(reject);
+//         });
+//     }
+//
+//
+//     function getDataset() {
+//
+//         if (this.dataset) {
+//             return Promise.resolve(this.dataset);
+//         }
+//         else {
+//             return this.reader.loadDataset();
+//         }
+//
+//     }
+//
+//
+//     return hic;
+// })(hic || {});
