49648e080a4b2b6767d441806d531122f5f60cd7
Douglass Turner <douglass.turner@gmail.com>
2018-03-01 18:18:14 -0500

    nav-bar. locus goto now beside resolution widget.

--- js/layoutController.js
+++ js/layoutController.js
@@ -135,8 +135,8 @@ var hic = (function (hic) {
         browser.locusGoto = new hic.LocusGoto(browser, $upper_widget_container);
 
         // resolution widget
-        // browser.resolutionSelector = new hic.ResolutionSelector(browser, $upper_widget_container);
-        // browser.resolutionSelector.setResolutionLock(browser.resolutionLocked);
+        browser.resolutionSelector = new hic.ResolutionSelector(browser, $upper_widget_container);
+        browser.resolutionSelector.setResolutionLock(browser.resolutionLocked);
 
         if (true === browser.config.figureMode) {
             browser.$contactMaplabel.addClass('hidden-text');
@@ -158,8 +158,8 @@ var hic = (function (hic) {
             browser.normalizationSelector = new hic.NormalizationWidget(browser, $lower_widget_container);
 
             // resolution widget
-            browser.resolutionSelector = new hic.ResolutionSelector(browser, $lower_widget_container);
-            browser.resolutionSelector.setResolutionLock(browser.resolutionLocked);
+            // browser.resolutionSelector = new hic.ResolutionSelector(browser, $lower_widget_container);
+            // browser.resolutionSelector.setResolutionLock(browser.resolutionLocked);
 
         }
 
