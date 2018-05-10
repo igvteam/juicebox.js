0ddfce4b05a27da6334f6059c57a0167b2281277
Jim Robinson <jrobinso@broadinstitute.org>
2018-05-10 16:35:44 -0700

    update build artifacts

--- examples/website/js/juicebox.js
+++ examples/website/js/juicebox.js
@@ -30478,15 +30478,11 @@ var hic = (function (hic) {
 
         function setDataRange() {
 
-            this.tile = undefined;
+            this.tile = undefined
 
-
-            if (min !== undefined) {
-                this.track.dataRange.min = min;
-            }
-
-            if (max !== undefined) {
-                this.track.dataRange.max = max;
+            this.track.dataRange = {
+                min: min,
+                max: max
             }
 
             this.track.autscale = autoscale;
