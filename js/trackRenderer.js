1515af52424da7a563403ef752435ce68cd728ee
Jim Robinson <jrobinso@broadinstitute.org>
2020-12-17 11:37:56 -0800

    track gear popover refactoring

--- js/trackRenderer.js
+++ js/trackRenderer.js
@@ -81,7 +81,7 @@ class TrackRenderer {
             this.track.trackView = this;
             this.track.trackView.trackDiv = this.$viewport.get(0);
 
-            appendRightHandGutter(this, this.$viewport);
+            this.appendRightHandGutter(this.$viewport);
 
             this.$viewport.on('click', function (e) {
                 e.preventDefault();
@@ -328,14 +328,15 @@ class TrackRenderer {
     repaintViews() {
         this.browser.renderTrackXY(this.trackRenderPair, true);
     }
+
+    appendRightHandGutter($parent) {
+        let $div = $('<div class="hic-igv-right-hand-gutter">')
+        $parent.append($div)
+        igv.TrackView.prototype.createTrackGearPopup.call(this, $div);
+    }
 }
 
-function appendRightHandGutter(trackView, $parent) {
-    const $div = $('<div class="hic-igv-right-hand-gutter">');
-    $parent.append($div);
-    igv.createTrackGearPopover(trackView, $div);
 
-}
 
 // ColorScaleWidget version of color picker
 function createColorPicker_ColorScaleWidget_version($parent, closeHandler, colorHandler) {
@@ -382,6 +383,17 @@ function identityTransformWithContext(context) {
     // 3x2 matrix. column major. (sx 0 0 sy tx ty).
     context.setTransform(1, 0, 0, 1, 0, 0);
 }
+
+
+function createCogIcon(name, color) {
+    color = color || "currentColor";
+    let icon = [512, 512, [], "f013", "M444.788 291.1l42.616 24.599c4.867 2.809 7.126 8.618 5.459 13.985-11.07 35.642-29.97 67.842-54.689 94.586a12.016 12.016 0 0 1-14.832 2.254l-42.584-24.595a191.577 191.577 0 0 1-60.759 35.13v49.182a12.01 12.01 0 0 1-9.377 11.718c-34.956 7.85-72.499 8.256-109.219.007-5.49-1.233-9.403-6.096-9.403-11.723v-49.184a191.555 191.555 0 0 1-60.759-35.13l-42.584 24.595a12.016 12.016 0 0 1-14.832-2.254c-24.718-26.744-43.619-58.944-54.689-94.586-1.667-5.366.592-11.175 5.459-13.985L67.212 291.1a193.48 193.48 0 0 1 0-70.199l-42.616-24.599c-4.867-2.809-7.126-8.618-5.459-13.985 11.07-35.642 29.97-67.842 54.689-94.586a12.016 12.016 0 0 1 14.832-2.254l42.584 24.595a191.577 191.577 0 0 1 60.759-35.13V25.759a12.01 12.01 0 0 1 9.377-11.718c34.956-7.85 72.499-8.256 109.219-.007 5.49 1.233 9.403 6.096 9.403 11.723v49.184a191.555 191.555 0 0 1 60.759 35.13l42.584-24.595a12.016 12.016 0 0 1 14.832 2.254c24.718 26.744 43.619 58.944 54.689 94.586 1.667 5.366-.592 11.175-5.459 13.985L444.788 220.9a193.485 193.485 0 0 1 0 70.2zM336 256c0-44.112-35.888-80-80-80s-80 35.888-80 80 35.888 80 80 80 80-35.888 80-80z"];
+    let svg = '<svg ' + 'viewBox="0 0 ' + icon[0] + ' ' + icon[1] + '">';
+    svg += '<path fill="' + color + '" ' + 'd="' + icon[4] + '">' + '</path>';
+    svg += '</svg>';
+    return $(svg);
+}
+
 
 
 export default TrackRenderer
