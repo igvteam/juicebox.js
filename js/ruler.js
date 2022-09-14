40c47978ef42c067fd8a918164829f9ae5f6f0bb
Douglass Turner <douglass.turner@gmail.com>
2022-09-14 16:50:59 -0400

    keep igv-canvas.js

--- js/ruler.js
+++ js/ruler.js
@@ -27,7 +27,7 @@
  */
 
 import {IGVColor, IGVMath} from '../node_modules/igv-utils/src/index.js'
-import igv from '../node_modules/igv/dist/igv.esm.js'
+import IGVGraphics from './igv-canvas.js'
 import $ from '../vendor/jquery-3.3.1.slim.js'
 
 function randomRGB(min, max) {
@@ -308,14 +308,14 @@ class Ruler {
         this.hideWholeGenome();
 
         identityTransformWithContext(this.ctx);
-        igv.IGVGraphics.fillRect(this.ctx, 0, 0, this.$canvas.width(), this.$canvas.height(), {fillStyle: IGVColor.rgbColor(255, 255, 255)});
+        IGVGraphics.fillRect(this.ctx, 0, 0, this.$canvas.width(), this.$canvas.height(), {fillStyle: IGVColor.rgbColor(255, 255, 255)});
 
         this.canvasTransform(this.ctx);
 
         w = ('x' === this.axis) ? this.$canvas.width() : this.$canvas.height();
         h = ('x' === this.axis) ? this.$canvas.height() : this.$canvas.width();
 
-        igv.IGVGraphics.fillRect(this.ctx, 0, 0, w, h, {fillStyle: IGVColor.rgbColor(255, 255, 255)});
+        IGVGraphics.fillRect(this.ctx, 0, 0, w, h, {fillStyle: IGVColor.rgbColor(255, 255, 255)});
 
         config.bpPerPixel = browser.dataset.bpResolutions[browser.state.zoom] / browser.state.pixelSize;
 
@@ -359,7 +359,7 @@ class Ruler {
 
         } else {
 
-            igv.IGVGraphics.fillRect(this.ctx, 0, 0, options.rulerLengthPixels, options.rulerHeightPixels, {fillStyle: IGVColor.rgbColor(255, 255, 255)});
+            IGVGraphics.fillRect(this.ctx, 0, 0, options.rulerLengthPixels, options.rulerHeightPixels, {fillStyle: IGVColor.rgbColor(255, 255, 255)});
 
             fontStyle = {
                 textAlign: 'center',
@@ -376,7 +376,7 @@ class Ruler {
 
             pixel = pixelLast = 0;
 
-            igv.IGVGraphics.setProperties(this.ctx, fontStyle);
+            IGVGraphics.setProperties(this.ctx, fontStyle);
             this.ctx.lineWidth = 1.0;
 
             yShim = 1;
@@ -412,7 +412,7 @@ class Ruler {
 
                         this.ctx.save();
                         this.labelReflectionTransform(this.ctx, pixel);
-                        igv.IGVGraphics.fillText(this.ctx, rulerLabel, pixel, options.height - (tickHeight / 0.75));
+                        IGVGraphics.fillText(this.ctx, rulerLabel, pixel, options.height - (tickHeight / 0.75));
                         this.ctx.restore();
 
                     }
@@ -422,7 +422,7 @@ class Ruler {
                 }
 
                 if (Math.floor((pixel * options.bpPerPixel) + options.bpStart) < chrSize) {
-                    igv.IGVGraphics.strokeLine(this.ctx,
+                    IGVGraphics.strokeLine(this.ctx,
                         pixel, options.height - tickHeight,
                         pixel, options.height - yShim);
                 }
@@ -432,7 +432,7 @@ class Ruler {
 
             } // while (pixel < options.rulerLengthPixels)
 
-            igv.IGVGraphics.strokeLine(this.ctx, 0, options.height - yShim, options.rulerLengthPixels, options.height - yShim);
+            IGVGraphics.strokeLine(this.ctx, 0, options.height - yShim, options.rulerLengthPixels, options.height - yShim);
 
         }
 
