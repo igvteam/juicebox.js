252d2a3215c34024334554256ac4ef9c39b434d6
turner <douglass.turner@gmail.com>
2024-10-14 12:11:08 -0400

    Fix typo

--- js/genomicUtils.js
+++ js/genomicUtils.js
@@ -6,7 +6,7 @@ function getLocus(dataset, state, width, height, bpPerPixel){
     const bpPerBin = bpPerPixel * state.pixelSize
 
     // bp = bin * bp / bin = bp
-    const xStarBP = 1 + Math.round(state.x * bpPerBin)
+    const xStartBP = 1 + Math.round(state.x * bpPerBin)
     const yStartBP = 1 + Math.round(state.y * bpPerBin)
 
     const chromosome1 = dataset.chromosomes[state.chr1]
@@ -18,18 +18,18 @@ function getLocus(dataset, state, width, height, bpPerPixel){
     const heightBP = Math.round(bpPerPixel * height)
 
     // bp = bp
-    const xEndBP = Math.min(chromosome1.size, widthBP + xStarBP - 1)
+    const xEndBP = Math.min(chromosome1.size, widthBP + xStartBP - 1)
     const yEndBP = Math.min(chromosome2.size, heightBP + yStartBP - 1)
 
-    return { xStarBP, yStartBP, xEndBP, yEndBP, chromosome1, chromosome2, pixelSize:state.pixelSize }
+    return { xStartBP, yStartBP, xEndBP, yEndBP, chromosome1, chromosome2, pixelSize:state.pixelSize }
 
 }
 
 function locusDescription(locus) {
 
-    const { xStarBP, yStartBP, xEndBP, yEndBP, chromosome1, chromosome2, pixelSize } = locus
+    const { xStartBP, yStartBP, xEndBP, yEndBP, chromosome1, chromosome2, pixelSize } = locus
 
-    const str1 = `${chromosome1.name}:${StringUtils.numberFormatter(xStarBP)}-${StringUtils.numberFormatter(xEndBP)}`
+    const str1 = `${chromosome1.name}:${StringUtils.numberFormatter(xStartBP)}-${StringUtils.numberFormatter(xEndBP)}`
     const str2 = `${chromosome2.name}:${StringUtils.numberFormatter(yStartBP)}-${StringUtils.numberFormatter(yEndBP)}`
 
     return `${ str1 } ${ str2 } pixelSize ${ pixelSize }`
